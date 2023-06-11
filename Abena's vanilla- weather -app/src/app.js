7dc502f8f8414789d6d63aa92772356e542cd9b8
Abena31 <129714347+Abena31@users.noreply.github.com>
2023-06-11 07:10:31 -0400

    updated time and temperature

--- Abena's vanilla- weather -app/src/app.js
+++ Abena's vanilla- weather -app/src/app.js
@@ -1,9 +1,43 @@
-function displayWeatherCondition(response) {
-  let temperatureElement = document.querySelector("#forecast");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+function formateDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = timestamp.getHours();
+  if (hours < 11) {
+    hours = "0${hours}";
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 11) {
+    minutes = "0${mintues}";
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    " Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return "${day} {hours}:${mintes}";
 }
-let apiKey = "e7846e44c0bd21b19tc86a51o0fef236";
-let apiEndpoint =
-  "https://api.shecodes.io/weather/v1/forecast?query={query}&key={key}";
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let sortElement = document.querySelector("#sort");
+  let moistElement = document.querySelector("#moist");
+  let airElement = document.querySelector("#air");
+  let gdateElement = document.querySelector("#gdate");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  sortElement.innerHTML = response.data.weather[0].sort;
+  moistElement.innerHTML = response.data.main.moist;
+  airElement.innerHTML = Math.round(response.data.air.speed);
+  gdateElement.innerHTML = formateDate(response.data.dt * 1000);
+}
 
-axios.get(apiEndpoint).then(displayWeatherCondition);
+let apiKey = "e8d4b7a751f8c833058726ca48c6c090";
+let apiEndpoint =
+  "https://api.openweathermap.org/data/2.5/weather?g=${city}&appid=${apiKey}&units=metric";
+
+axios.get(apiEndpoint).then(displayTemperature);
