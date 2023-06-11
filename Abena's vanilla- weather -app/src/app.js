a22528ec84a803342f4b6ac42bf15bc9c499f851
Abena31 <129714347+Abena31@users.noreply.github.com>
2023-06-10 21:41:58 -0400

    updated java script

--- Abena's vanilla- weather -app/src/app.js
+++ Abena's vanilla- weather -app/src/app.js
@@ -2,8 +2,8 @@ function displayWeatherCondition(response) {
   let temperatureElement = document.querySelector("#forecast");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
-let apiKey = "79e8e44340f805883833d9a47487d24b";
+let apiKey = "e7846e44c0bd21b19tc86a51o0fef236";
 let apiEndpoint =
-  "http://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric";
+  "https://api.shecodes.io/weather/v1/forecast?query={query}&key={key}";
 
 axios.get(apiEndpoint).then(displayWeatherCondition);
